98834065c211d92811c3288fc353fb70b3bb2536
hieunv6 <nguyenhieu17011@gmail.com>
2021-04-09 18:14:54 +0700

    init code

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -2,13 +2,28 @@
   <div class="left grid fixed header">
     <div>
       <nuxt-link to="/">
-      <img src='https://www.bornfight.com/wp-content/themes/bornfight-web-2020/static/ui/BF-sign-light.svg' alt='Born-fight' class='w-full mt-4 header-logo' /> 
-    </nuxt-link>
+        <img
+          src="https://www.bornfight.com/wp-content/themes/bornfight-web-2020/static/ui/BF-sign-light.svg"
+          alt="Born-fight"
+          class="w-full mt-4 header-logo"
+        />
+      </nuxt-link>
     </div>
-    <div class='text-center cursor-pointer header-menu' :class="{'open':menuOpen}" @click="openMenu">
-      <i class='header-menu-line line-top'></i>
-      <i class='header-menu-line line-middle'></i>
-      <i class='header-menu-line line-bot'></i>
+    <div
+      id="icon-toggle"
+      class="text-center cursor-pointer header-menu"
+      :class="{ open: menuOpen }"
+      @click="openMenu"
+    >
+      <i class="header-menu-line line-top"></i>
+      <i class="header-menu-line line-middle"></i>
+      <i class="header-menu-line line-bot"></i>
+    </div>
+    <div class="navigation">
+      <div class="navigation__content">
+        <div class="navigation__content-left"></div>
+        <div class="navigation__content-right"></div>
+      </div>
     </div>
     <!-- <div>
       hello@bornfight.com
@@ -20,17 +35,35 @@ export default {
   name: "Header",
   data() {
     return {
-      menuOpen: false,
+      menuOpen: false
     };
   },
   methods: {
     openMenu() {
       this.menuOpen = !this.menuOpen;
-    },
-  },
+    }
+  }
 };
 </script>
 <style scoped>
+#icon-toggle {
+  z-index: 999;
+}
+.navigation {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #fff;
+  height: 100vh;
+  /* display: none; */
+}
+.navigation__content {
+}
+.navigation__content-left {
+}
+.navigation__content-right {
+}
 .header {
   width: 100px;
   height: 100vh;
@@ -45,6 +78,7 @@ export default {
   width: 2px;
   height: 30px;
   transition: 0.4s;
+  mix-blend-mode: difference;
 }
 .open .line-top {
   transform: rotate(-45deg) translate(0px, 0px);
