5e1279f725dfe2c3cc333eb3c1d6c81dec9cd504
duongnt <duongnt247@gmail.com>
2021-04-14 16:28:17 +0700

    delete webpack file

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-import { build } from "vue-webpack";
-
-const config = build({
-  env: "development",
-  mode: "server",
-  inputFilePath: "./src/client/server-entry.js",
-  outputFileName: "bundle",
-  outputPath: "./dist"
-});
